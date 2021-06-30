d19ae131fc70cf520515ede072e2984d6c809d88
okiwa86 <okiwa86>
2021-06-30 15:42:07 +0900

    add

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1117,92 +1117,6 @@ public class InAppBrowser extends CordovaPlugin {
         }
 
 
-        private boolean url_scheme_intent( WebView view, String url )
-        {
-            Log.d( SampleApplication.m_strLogTag, "[PayDemoActivity] called__test - url=[" + url + "]" );
-
-            //chrome 버젼 방식 : 2014.01 추가
-            if ( url.startsWith( "intent" ) )
-            {
-                Intent intent = null;
-
-                try {
-                    intent = Intent.parseUri( url, Intent.URI_INTENT_SCHEME );
-                } catch ( URISyntaxException ex ) {
-                    Log.d( SampleApplication.m_strLogTag, "[PayDemoActivity] URISyntaxException=[" + ex.getMessage() + "]" );
-                    return false;
-                }
-
-                // 앱설치 체크를 합니다.
-                if ( cordova.getActivity().getPackageManager().resolveActivity( intent, 0 ) == null )
-                {
-                    String packagename = intent.getPackage();
-
-                    if ( packagename != null )
-                    {
-                        cordova.getActivity().startActivity( new Intent( Intent.ACTION_VIEW, Uri.parse( "market://search?q=pname:" + packagename ) ) );
-
-                        return true;
-                    }
-                }
-
-                intent = new Intent( Intent.ACTION_VIEW, Uri.parse( intent.getDataString() ) );
-
-                try{
-                    cordova.getActivity().startActivity( intent );
-                }catch( ActivityNotFoundException e ) {
-                    Log.d( SampleApplication.m_strLogTag, "[PayDemoActivity] ActivityNotFoundException=[" + e.getMessage() + "]" );
-                    return false;
-                }
-            }
-            // 기존 방식
-            else
-            {
-            /*
-             * 아래 주석된 내용은 업체에서 필요에 따라 설정하는 부분입니다.
-             * KCP에서는 웹에서 지원될수 있도록 처리가 되어 있습니다.
-             * 어플에서 판단이 필요할경우 추가해주세요.
-             * 단. 아래 주석 내용은 공지없이 KCP 에서 변경이 가능합니다.
-             *
-            if ( url.startsWith( "ispmobile" ) )
-            {
-                if( !new PackageState( this ).getPackageDownloadInstallState( "kvp.jjy.MispAndroid" ) )
-                {
-                    startActivity( new Intent(Intent.ACTION_VIEW, Uri.parse( "market://details?id=kvp.jjy.MispAndroid320" ) ) );
-
-                    return true;
-                }
-            }
-
-            // 삼성과 같은 경우 어플이 없을 경우 마켓으로 이동 할수 있도록 넣은 샘플 입니다.
-            // 실제 구현시 업체 구현 여부에 따라 삭제 처리 하시는것이 좋습니다.
-            if ( url.startsWith( "mpocket.online.ansimclick" ) )
-            {
-                if( !new PackageState( this ).getPackageDownloadInstallState( "kr.co.samsungcard.mpocket" ) )
-                {
-                    Toast.makeText(this, "어플을 설치 후 다시 시도해 주세요.", Toast.LENGTH_LONG).show();
-
-                    startActivity( new Intent(Intent.ACTION_VIEW, Uri.parse( "market://details?id=kr.co.samsungcard.mpocket" ) ) );
-
-                    return true;
-                }
-            }
-            */
-
-                try
-                {
-                    cordova.getActivity().startActivity( new Intent( Intent.ACTION_VIEW, Uri.parse( url ) ) );
-                }
-                catch(Exception e)
-                {
-                    // 어플이 설치 안되어 있을경우 오류 발생. 해당 부분은 업체에 맞게 구현
-                    Toast.makeText(this, "해당 어플을 설치해 주세요.", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            return true;
-        }
-
         /**
          * Override the URL that should be loaded
          *
@@ -1215,40 +1129,7 @@ public class InAppBrowser extends CordovaPlugin {
         @SuppressWarnings("deprecation")
         @Override
         public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-
-            if (url != null && !url.equals("about:blank"))
-            {
-                if( url.startsWith("http://") || url.startsWith("https://"))
-                {
-                    if (url.contains("http://market.android.com")            ||
-                            url.contains("http://m.ahnlab.com/kr/site/download") ||
-                            url.endsWith(".apk")                                   )
-                    {
-                        return url_scheme_intent( view, url );
-                    }
-                    else
-                    {
-                        view.loadUrl( url );
-                    }
-                }
-                else if(url.startsWith("mailto:"))
-                {
-                    return false;
-                }
-                else if(url.startsWith("tel:"))
-                {
-                    return false;
-                }
-                else
-                {
-                    return url_scheme_intent( view, url );
-                }
-            }
-
-            return true;
-
-
-            // return shouldOverrideUrlLoading(url, null);
+            return shouldOverrideUrlLoading(url, null);
         }
 
         /**
@@ -1323,7 +1204,7 @@ public class InAppBrowser extends CordovaPlugin {
                 } catch (android.content.ActivityNotFoundException e) {
                     LOG.e(LOG_TAG, "Error dialing " + url + ": " + e.toString());
                 }
-            } else if (url.startsWith("geo:") || url.startsWith(WebView.SCHEME_MAILTO) || url.startsWith("market:") || url.startsWith("intent:")) {
+            } else if (url.startsWith("geo:") || url.startsWith(WebView.SCHEME_MAILTO) || url.startsWith("market:")) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
                     intent.setData(Uri.parse(url));
@@ -1331,6 +1212,30 @@ public class InAppBrowser extends CordovaPlugin {
                     override = true;
                 } catch (android.content.ActivityNotFoundException e) {
                     LOG.e(LOG_TAG, "Error with " + url + ": " + e.toString());
+                }
+            }
+            else if (url.startsWith("intent:")) {
+                try {
+                    intent = Intent.parseUri( url, Intent.URI_INTENT_SCHEME );
+                    override = true;
+                } catch ( URISyntaxException ex ) {
+                    LOG.e(LOG_TAG, "[PayDemoActivity] URISyntaxException=[" + ex.getMessage() + "]" );
+                    return false;
+                }
+
+                if ( cordova.getActivity().getPackageManager().resolveActivity( intent, 0 ) == null )
+                {
+                    String packagename = intent.getPackage();
+                    if ( packagename != null ) {
+                        cordova.getActivity().startActivity( new Intent( Intent.ACTION_VIEW, Uri.parse( "market://search?q=pname:" + packagename ) ) );
+                        return true;
+                    }
+                }
+                intent = new Intent( Intent.ACTION_VIEW, Uri.parse( intent.getDataString() ) );
+                try{
+                    cordova.getActivity().startActivity( intent );
+                }catch( ActivityNotFoundException e ) {
+                    Log.d( SampleApplication.m_strLogTag, "[PayDemoActivity] ActivityNotFoundException=[" + e.getMessage() + "]" );
                 }
             }
             // If sms:5551212?body=This is the message
